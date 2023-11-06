c6eb0b7dfbddb6c12be7a0acf8e3bb61f6623938
02loveslollipop <mmonsalve2005@gmail.com>
2023-11-06 01:31:34 -0500

    Add foodRequest to AI model

--- lib/login.dart
+++ lib/login.dart
@@ -5,12 +5,13 @@ import 'package:front_is/menu.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Login extends StatelessWidget {
-
   Future<void> authRequest(String user, String password, context) async {
     final client = http.Client();
-    final url = Uri.parse('http://127.0.0.1:6970/login'); //TODO: made a better implementation, not hardcoded
+    final url = Uri.parse(
+        'http://127.0.0.1:6970/login'); //TODO: made a better implementation, not hardcoded
 
     try {
       final response = await client.get(
@@ -21,9 +22,34 @@ class Login extends StatelessWidget {
         },
       );
       if (response.statusCode == 200) {
+        Map<String, dynamic> json = jsonDecode(response.body);
+        
+        String secretAuth = json['secretAuth'] ?? "ERROR";
+        if(secretAuth == 0){
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("ERROR, THIS DID'T WORK AS  EXPECTED :("),
+                content: const Text(
+                    "Could not fetch the secretAuth. Please try again."),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                        'OK'),
+                  ),
+                ],
+              );
+            },
+          );
+          return;
+        }
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => Menu(user)),
+          MaterialPageRoute(builder: (context) => Menu(user, secretAuth)),
         );
       } else {
         showDialog(
@@ -31,13 +57,15 @@ class Login extends StatelessWidget {
           builder: (BuildContext context) {
             return AlertDialog(
               title: const Text('ALERT'),
-              content: const Text("As you can see, you entered a wrong username or password. Please try again. and please don't try to hack me :)"),
+              content: const Text(
+                  "As you can see, you entered a wrong username or password. Please try again. and please don't try to hack me :)"),
               actions: <Widget>[
                 TextButton(
                   onPressed: () {
                     Navigator.of(context).pop();
                   },
-                  child: const Text('OK, I understand, I will not try to hack you :)'),
+                  child: const Text(
+                      'OK, I understand, I will not try to hack you :)'),
                 ),
               ],
             );
@@ -45,23 +73,26 @@ class Login extends StatelessWidget {
         );
       }
     } catch (e) {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('ERROR, THIS S**T DID NOT WORK AS  EXPECTED :('),
-                    content:Text("Something went wrong. Please try again. and please don't try to hack me :)" + e.toString()),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('OK, I understand, I will not try to hack you :)'),
-                      ),
-                    ],
-                  );
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('ERROR, THIS S**T DID NOT WORK AS  EXPECTED :('),
+            content: Text(
+                "Something went wrong. Please try again. and please don't try to hack me :)" +
+                    e.toString()),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
                 },
-              );
+                child: const Text(
+                    'OK, I understand, I will not try to hack you :)'),
+              ),
+            ],
+          );
+        },
+      );
     }
   }
 
@@ -120,7 +151,8 @@ class Login extends StatelessWidget {
                 SizedBox(height: 40),
                 Container(
                   width: 400,
-                  child: TextField( //when this textfield is filled, the user is extracted
+                  child: TextField(
+                    //when this textfield is filled, the user is extracted
                     decoration: InputDecoration(
                       hintText: "Usuario",
                       fillColor: Colors.white,
@@ -128,7 +160,8 @@ class Login extends StatelessWidget {
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(15),
                       ),
-                    ), onChanged: (value) => username = value,
+                    ),
+                    onChanged: (value) => username = value,
                   ),
                 ),
                 SizedBox(height: 20),
@@ -143,14 +176,15 @@ class Login extends StatelessWidget {
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(15),
                       ),
-                    ), onChanged: (value) => password = value,
+                    ),
+                    onChanged: (value) => password = value,
                   ),
                 ),
                 SizedBox(height: 40),
                 ElevatedButton(
                   onPressed: () {
                     //TODO: call the authRequest function
-                    //extract user from the first textfield 
+                    //extract user from the first textfield
                     //extract password from the second textfield
                     //call the authRequest function
                     authRequest(username, password, context);
--- lib/menu.dart
+++ lib/menu.dart
@@ -3,15 +3,121 @@ import 'package:front_is/load.dart';
 import 'package:front_is/usuario.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 class Menu extends StatefulWidget {
+  String username = "";
+  String secret = "";
 
-  String username = ""; 
-  
-  Menu(user, {Key? key}): super(key: key){
+  Menu(String user,String secretAuth, {Key? key}) : super(key: key) {
     username = user;
+    secret = secretAuth;
   }
-  
+
+  Future<void> foodRequest(
+      String ingredients, int ingredientListLenght, context) async {
+    dynamic client = http.Client();
+    final url = Uri.parse(
+        'http://127.0.0.1:6970/create_recipe'); //TODO: made a better implementation, not hardcoded
+    String ingredients = "";
+
+    if (ingredientListLenght == 0) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('ALERT >:('),
+            content: const Text(
+                "As you can see, You didn't select any ingredien at all. So we will not make any recommendation. If you want a recommendation, please select at least one ingredient. :)"),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text(
+                    'OK, I understand, I will use at least one ingredient :)'),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
+    Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const Load()),
+        );
+    try {
+      final response = await client.get(
+        url,
+        headers: <String, String>{
+          'secretAuth': secret,
+          'ingredients': ingredients,
+        },
+      );
+      if (response.statusCode == 200) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('RESULT'),
+              content: Text(
+                  "Result: \n" + response.body.toString()),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK :('),
+                ),
+              ],
+            );
+          },
+        );
+        //when the response is 200, we will go to the result page
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('ALERT'),
+              content: const Text(
+                  "Something unexpected happened. We will take you back to the selection menu, SORRY!!!"),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK :('),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('ERROR, THIS S**T DID NOT WORK AS  EXPECTED :('),
+            content: Text("Something went wrong. Please try again. SORRY!!!" +
+                e.toString()),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK :('),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
   @override
   _MenuState createState() => _MenuState();
 }
@@ -44,7 +150,7 @@ class _MenuState extends State<Menu> {
                   borderRadius: BorderRadius.circular(20.0),
                 ),
               ),
-              child: Text("Hola, Usuario",
+              child: Text("Hola, ${widget.username}",
                   style: GoogleFonts.montserrat(
                     color: Colors.black,
                     fontWeight: FontWeight.bold,
@@ -116,12 +222,22 @@ class _MenuState extends State<Menu> {
                   padding: const EdgeInsets.symmetric(vertical: 8.0),
                   child: ElevatedButton(
                     onPressed: () {
+                      String ingredients = "";
+                      for(int i = 0; i < ingredientSelected.length; i++){
+                        if(ingredientSelected[i]){
+                          ingredients += getIngredientName(i) + ",";
+                        }
+                      }
+                      print(ingredients); //TODO: delete this shit
+                      widget.foodRequest(ingredients, ingredientSelected.length, context);
+                      //TODO: here is the logic when pressed
+                      /*
                       Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => Load(),
                         ),
-                      );
+                      );*/
                     },
                     style: ElevatedButton.styleFrom(
                       primary: Color.fromARGB(255, 208, 33, 20),
